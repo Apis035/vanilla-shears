6ba7e7b5db443f254e0d1b86e9820dd6e61cc5b8
Cocoa <gd.apis035@gmail.com>
2022-01-08 09:09:24 +0700

    update

---
+++ src/main/java/net/apis035/vanillashears/ModShearsItem.java
@@ -0,0 +1,35 @@
+package net.apis035.vanillashears;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.event.GameEvent;
+
+public class ModShearsItem extends ShearsItem {
+    public ModShearsItem(Settings settings, ToolMaterials material) {
+        super(settings.group(ItemGroup.TOOLS).maxDamage(material.getDurability()-12));
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        if (entity.world.isClient)
+            return ActionResult.PASS;
+
+        if (entity instanceof SheepEntity sheepEntity) {
+            if (sheepEntity.isShearable()) {
+                sheepEntity.sheared(SoundCategory.PLAYERS);
+                sheepEntity.emitGameEvent(GameEvent.SHEAR, user);
+                stack.damage(1, user, (p) -> p.sendToolBreakStatus(hand));
+                return ActionResult.SUCCESS;
+            } else {
+                return ActionResult.CONSUME;
+            }
+        }
+        return ActionResult.PASS;
+    }
+}
---
+++ src/main/java/net/apis035/vanillashears/VanillaShears.java
@@ -0,0 +1,59 @@
+package net.apis035.vanillashears;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.tag.TagFactory;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.*;
+import net.minecraft.loot.condition.MatchToolLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.predicate.item.ItemPredicate;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class VanillaShears implements ModInitializer {
+    public static final String MOD_ID = "vanillashears";
+
+    public static final Item WOODEN_SHEARS = new ModShearsItem(new Item.Settings(), ToolMaterials.WOOD);
+    public static final Item STONE_SHEARS = new ModShearsItem(new Item.Settings(), ToolMaterials.STONE);
+    public static final Item GOLDEN_SHEARS = new ModShearsItem(new Item.Settings(), ToolMaterials.GOLD);
+    public static final Item DIAMOND_SHEARS = new ModShearsItem(new Item.Settings(), ToolMaterials.DIAMOND);
+    public static final Item NETHERITE_SHEARS = new ModShearsItem(new Item.Settings(), ToolMaterials.NETHERITE);
+
+    public static final Tag<Item> SHEARS = TagFactory.ITEM.create(new Identifier("c", "shears"));
+
+    public static void register(String id, Item item) {
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, id), item);
+    }
+
+    @Override
+    public void onInitialize() {
+        register("wooden_shears", WOODEN_SHEARS);
+        register("stone_shears", STONE_SHEARS);
+        register("golden_shears", GOLDEN_SHEARS);
+        register("diamond_shears", DIAMOND_SHEARS);
+        register("netherite_shears", NETHERITE_SHEARS);
+
+        Block[] drops = {
+                Blocks.OAK_LEAVES, Blocks.SPRUCE_LEAVES, Blocks.BIRCH_LEAVES,
+                Blocks.JUNGLE_LEAVES, Blocks.ACACIA_LEAVES, Blocks.DARK_OAK_LEAVES,
+                Blocks.AZALEA_LEAVES, Blocks.FLOWERING_AZALEA_LEAVES};
+
+        for (Block drop : drops) {
+            FabricLootPoolBuilder pool = FabricLootPoolBuilder.builder()
+                    .rolls(ConstantLootNumberProvider.create(1))
+                    .with(ItemEntry.builder(drop))
+                    .withCondition(new MatchToolLootCondition(ItemPredicate.Builder.create().tag(SHEARS).build()));
+
+            LootTableLoadingCallback.EVENT.register((resourceManager, manager, id, supplier, setter) -> {
+                if (drop.getLootTableId().equals(id)) {
+                    supplier.pool(pool);
+                }
+            });
+        }
+    }
+}
